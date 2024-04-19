16e33a7c3b7981263b7456a4085ae451a96111e4
vandinh52 <157068699+vandinh52@users.noreply.github.com>
2024-04-19 19:29:57 +0000

    update carousel component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,9 @@
+import PropTypes from 'prop-types';
+import { image_uri } from "../Request";
+import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
+import { useEffect } from 'react';
 
-function Carousel() {
+function Carousel(props) {
     return <h1> Carousel </h1>
 }
 
