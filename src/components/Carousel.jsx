8f1b3690f552b375b39914ead399c016af0552f1
vandinh52 <157068699+vandinh52@users.noreply.github.com>
2024-04-19 19:30:50 +0000

    update carousel component and props

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,7 +4,57 @@ import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 import { useEffect } from 'react';
 
 function Carousel(props) {
-    return <h1> Carousel </h1>
+    useEffect(() => {
+        // This code runs after the component has rendered
+        const slider = document.getElementById(`slider-${props.sectionID}`);
+        if (slider) {
+            // Access the slider element and perform any necessary actions
+        }
+    }, [props.sectionID, props.slideSize]); // Run this effect whenever sectionID changes
+    const prev = () => {
+        const slider = document.getElementById(`slider-${props.sectionID}`);
+        if (!slider) return (slider);
+        if (slider.offsetWidth < 625) {
+            slider.scrollLeft = slider.scrollLeft - slider.offsetWidth;
+        } else if (slider.offsetWidth < 753) {
+            slider.scrollLeft = slider.scrollLeft - slider.offsetWidth / 2;
+        } else {
+            slider.scrollLeft = slider.scrollLeft - slider.offsetWidth / props.slideSize;
+        }
+    };
+
+    const next = () => {
+
+        const slider = document.getElementById(`slider-${props.sectionID}`);
+        if (!slider) return (slider);
+        if (slider.offsetWidth < 625) {
+            slider.scrollLeft = slider.scrollLeft + slider.offsetWidth;
+        } else if (slider.offsetWidth < 753) {
+            slider.scrollLeft = slider.scrollLeft + slider.offsetWidth / 2;
+        } else {
+            slider.scrollLeft = slider.scrollLeft + slider.offsetWidth / props.slideSize;
+        }
+    };
+
+    return (
+        <div className='relative overflow-hidden group'>
+            <div id={`slider-${props.sectionID}`} className="slider flex -ml-4 overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide">
+                {props.movieList.map((movie, index) => {
+                    return (
+                        <div key={index} className={`slide min-w-0 shrink-0 grow-0 basis-full pl-4 rounded-[10px] shadow-custom md:basis-1/2 ${(props.slideSize === 4) ? 'lg:basis-1/4' : 'lg:basis-1/3'}`}>
+                            <img src={`${image_uri}/w500/${movie.backdrop_path}`} className='w-full' alt="" />
+                        </div>
+                    );
+                })}
+            </div>
+            <button onClick={prev} className='bg-black/5 group-hover:bg-black/50 text-white z-10 absolute top-0 left-0 bottom-0'>
+                <BiChevronLeft size={30} className='invisible group-hover:visible' />
+            </button>
+            <button onClick={next} className='bg-black/10 group-hover:bg-black/50 text-white z-10 absolute top-0 right-0 bottom-0'>
+                <BiChevronRight size={30} className='invisible group-hover:visible' />
+            </button>
+        </div>
+    );
 }
 
 export default Carousel;
