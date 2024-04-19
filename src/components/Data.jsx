b661478924632180556b21cb8ee759e9ac0b393a
vandinh52 <157068699+vandinh52@users.noreply.github.com>
2024-04-19 19:44:01 +0000

    Data file to hold data for not repeat code

---
+++ src/components/Data.jsx
@@ -0,0 +1,172 @@
+const tmdbMovies = [
+    {
+        adult: false,
+        backdrop_path: "/1XDDXPXGiI8id7MrUxK36ke7gkX.jpg",
+        genre_ids: [28, 12, 16, 35, 10751],
+        id: 1011985,
+        original_language: "en",
+        original_title: "Kung Fu Panda 4",
+        overview: "Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.",
+        popularity: 4327.321,
+        poster_path: "/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg",
+        release_date: "2024-03-02",
+        title: "Kung Fu Panda 4",
+        video: false,
+        vote_average: 6.9,
+        vote_count: 433
+    },
+    {
+        adult: false,
+        backdrop_path: "/1XDDXPXGiI8id7MrUxK36ke7gkX.jpg",
+        genre_ids: [28, 12, 16, 35, 10751],
+        id: 1011985,
+        original_language: "en",
+        original_title: "Kung Fu Panda 5",
+        overview: "Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.",
+        popularity: 4327.321,
+        poster_path: "/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg",
+        release_date: "2024-03-02",
+        title: "Kung Fu Panda 4",
+        video: false,
+        vote_average: 6.9,
+        vote_count: 433
+    },
+    {
+        adult: false,
+        backdrop_path: "/1XDDXPXGiI8id7MrUxK36ke7gkX.jpg",
+        genre_ids: [28, 12, 16, 35, 10751],
+        id: 1011985,
+        original_language: "en",
+        original_title: "Kung Fu Panda 6",
+        overview: "Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.",
+        popularity: 4327.321,
+        poster_path: "/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg",
+        release_date: "2024-03-02",
+        title: "Kung Fu Panda 4",
+        video: false,
+        vote_average: 6.9,
+        vote_count: 433
+    },
+    {
+        adult: false,
+        backdrop_path: "/1XDDXPXGiI8id7MrUxK36ke7gkX.jpg",
+        genre_ids: [28, 12, 16, 35, 10751],
+        id: 1011985,
+        original_language: "en",
+        original_title: "Kung Fu Panda 7",
+        overview: "Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.",
+        popularity: 4327.321,
+        poster_path: "/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg",
+        release_date: "2024-03-02",
+        title: "Kung Fu Panda 4",
+        video: false,
+        vote_average: 6.9,
+        vote_count: 433
+    },
+    {
+        adult: false,
+        backdrop_path: "/1XDDXPXGiI8id7MrUxK36ke7gkX.jpg",
+        genre_ids: [28, 12, 16, 35, 10751],
+        id: 1011985,
+        original_language: "en",
+        original_title: "Kung Fu Panda 8",
+        overview: "Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.",
+        popularity: 4327.321,
+        poster_path: "/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg",
+        release_date: "2024-03-02",
+        title: "Kung Fu Panda 4",
+        video: false,
+        vote_average: 6.9,
+        vote_count: 433
+    },
+]
+
+const genres = {
+    "genres": [
+        {
+            "id": 28,
+            "name": "Action"
+        },
+        {
+            "id": 12,
+            "name": "Adventure"
+        },
+        {
+            "id": 16,
+            "name": "Animation"
+        },
+        {
+            "id": 35,
+            "name": "Comedy"
+        },
+        {
+            "id": 80,
+            "name": "Crime"
+        },
+        {
+            "id": 99,
+            "name": "Documentary"
+        },
+        {
+            "id": 18,
+            "name": "Drama"
+        },
+        {
+            "id": 10751,
+            "name": "Family"
+        },
+        {
+            "id": 14,
+            "name": "Fantasy"
+        },
+        {
+            "id": 36,
+            "name": "History"
+        },
+        {
+            "id": 27,
+            "name": "Horror"
+        },
+        {
+            "id": 10402,
+            "name": "Music"
+        },
+        {
+            "id": 9648,
+            "name": "Mystery"
+        },
+        {
+            "id": 10749,
+            "name": "Romance"
+        },
+        {
+            "id": 878,
+            "name": "Science Fiction"
+        },
+        {
+            "id": 10770,
+            "name": "TV Movie"
+        },
+        {
+            "id": 53,
+            "name": "Thriller"
+        },
+        {
+            "id": 10752,
+            "name": "War"
+        },
+        {
+            "id": 37,
+            "name": "Western"
+        }
+    ]
+}
+
+const navItems = [
+    { link: "Movies", path: "/" },
+    { link: "Series", path: "/series" },
+    { link: "Shows", path: "/shows" },
+    { link: "Be a Chiller", path: "/be-a-chiller" },
+];
+
+export { tmdbMovies, genres, navItems };
